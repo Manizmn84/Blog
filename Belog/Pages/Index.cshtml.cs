0dc49032d946447b0cc7b4599a2e8c8fd8f2cd41
mani <mani.84.zmn@gmail.com>
2025-07-10 16:45:13 +0330

    solve the login template

--- Belog/Pages/Index.cshtml.cs
+++ Belog/Pages/Index.cshtml.cs
@@ -1,7 +1,12 @@
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-namespace Belog.Pages
+namespace CodeYad_Blog.Web.Pages
 {
     public class IndexModel : PageModel
     {
